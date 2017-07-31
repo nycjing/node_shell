43081ead559dab0291f5a0df0bc256c8ae395685
Jing Jia <nycjing@gmail.com>
2017-07-31 13:45:25 -0400

    add done function

--- commands.js
+++ commands.js
@@ -2,63 +2,76 @@ var fs = require('fs');
 var request = require('request');
 
 module.exports = {
+    done: function(output, withPromp){
+        if ( withPromp === null )
+        {
+            process.stdout.write(output + "\nprompt>");
+            process.stdout.write("prompt>");
+        }
+        else
+            process.stdout.write(output + '\n');
+    },
     pwd: function () {
-        process.stdout.write(`Current directory: ${process.cwd()}` + '\n');
-        process.stdout.write('\nprompt>');
+        this.done(`${process.cwd()}`);
+
     },
 
     date: function(){
-        process.stdout.write(Date()+'\n');
-        process.stdout.write('\nprompt>');
+        this.done(Date());
     },
 
     ls: function(){
+        var done = this.done;
         fs.readdir('.', function(err, files) {
             if (err) throw err;
             files.forEach(function(file) {
-                process.stdout.write(file.toString() + "\n");
+                done(file.toString(),false);
             })
             process.stdout.write("prompt > ");
         });
     },
 
     echo: function(arg){
-        process.stdout.write(arg + '\n');
-        process.stdout.write('\nprompt>');
+        this.done(arg);
     },
 
     cat: function (filename) {
         var fullName = `${process.cwd()}/`+filename;
+        var done = this.done;
         fs.readFile(fullName.trim(), function (err, data) {
             if (err) throw err;
-            process.stdout.write(data + '\nprompt>');
+            done(data);
         });
     },
 
     head: function(filename){
         var fullName = `${process.cwd()}/`+filename;
+        var done = this.done;
         fs.readFile(fullName.trim(), function (err, data) {
             if (err) throw err;
             var fileOutput = data.toString().split('\n').slice(0,5).join('\n');
-            process.stdout.write(fileOutput + '\nprompt>');
+            done(fileOutput);
         });
     },
 
     tail: function(filename){
         var fullName = `${process.cwd()}/`+filename;
+        var done = this.done;
         fs.readFile(fullName.trim(), function (err, data) {
             if (err) throw err;
             var fileOutput = data.toString().split('\n').slice(-5).join('\n');
-            process.stdout.write(fileOutput + '\nprompt>');
+            done(fileOutput);
         });
     },
 
     curl: function(link){
-
+        var done = this.done;
         request(link, function(err, im, body){
             if (err) throw err;
-            process.stdout.write(body + '\nprompt>');
+            done(body);
         });
 
     }
+
+
 };
